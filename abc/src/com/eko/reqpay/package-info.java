1f793f62a8fc1ecc1945f3ec07b4c178791dd06a
Kumar Saurav <ksaurav.cruise@gmail.com>
2016-09-11 18:45:57 +0530

    2nd commit

--- abc/src/com/eko/reqpay/package-info.java
+++
@@ -1,7 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-		namespace = "http://nameSpaceUri",
-            xmlns = { @XmlNs(prefix = "upi", namespaceURI = "http://nameSpaceUri") },
-		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.UNSET)
-package com.eko.reqpay;
-import javax.xml.bind.annotation.XmlNs;
-
